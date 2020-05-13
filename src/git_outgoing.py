02a8174a01d9d67f288838a020c51dcf0e5928c2
Jonathan DeCarlo <jddecarlo@gmail.com>
2020-05-13 11:58:28 -0400

    Updated the incoming and outgoing files to fetch first.

--- src/git_outgoing.py
+++ src/git_outgoing.py
@@ -8,6 +8,10 @@ not in the remote target branch.
 import git_shared
 import sys
 
+rc, output = git_shared.run_command("git fetch")
+if rc != 0:
+    print("Error: Couldn't fetch latest from remote.\n")
+    sys.exit(rc)
 rc, local_branch_name, remote_branch_name = git_shared.get_current_branch_info()
 if rc != 0 or len(local_branch_name) == 0 or len(remote_branch_name) == 0:
     sys.exit(rc)
@@ -16,5 +20,7 @@ rc, output = git_shared.run_command(command)
 if rc != 0:
     print("Error: Couldn't get difference between local and remote branches.\n")
     sys.exit(4)
-if len(output) > 0:
+elif len(output) == 0:
+    print("No outgoing changesets found. The remote is up to date.\n")
+else:
     print("".join(output))
