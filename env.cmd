ae221edc623c0139720413f6e24647d720540fbf
Jonathan DeCarlo <jddecarlo@gmail.com>
2020-05-13 12:28:00 -0400

    Added environment file to set helpful git aliases.

---
+++ env.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+git config --global alias.bdiff "difftool --dir-diff --no-symlinks"
+git config --global alias.restore "checkout --"
+git config --global alias.restoreall "reset --hard"
+git config --global alias.alias "config --get-regexp alias"
+git config --global alias.updateremotes "remote update origin --prune"
+git config --global alias.unstage "reset HEAD"
+git config --global alias.incoming "!python $COMMAND_UTILITIES_PATH\\src\\git_incoming.py"
+git config --global alias.outgoing "!python $COMMAND_UTILITIES_PATH\\src\\git_outgoing.py"
+git config --global alias.in "incoming"
+git config --global alias.out "outgoing"
